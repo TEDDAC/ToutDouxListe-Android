949e72d0390e4891e25fa4d23b1162277bde1c5a
TEDDAC <mathisribemont@gmail.com>
2024-12-18 18:45:00 +0100

    better code for date picker

--- app/src/main/java/com/teddac/toutdouxliste/ui/page/EditPage.kt
+++ app/src/main/java/com/teddac/toutdouxliste/ui/page/EditPage.kt
@@ -1,5 +1,6 @@
 package com.teddac.toutdouxliste.ui.page
 
+import android.app.TimePickerDialog
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
@@ -9,14 +10,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -40,14 +44,7 @@ fun EditPage(){
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
     val datePickerState = rememberDatePickerState()
-    val dueDateStr = LocalDateTime.ofInstant(
-        Instant.ofEpochMilli(
-            datePickerState.selectedDateMillis ?: Instant.now().toEpochMilli()
-        ),
-        ZoneId.systemDefault()
-    ).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))
-
-    var showDatePicker by remember { mutableStateOf(false) }
+    val timePickerState = rememberDatePickerState()
 
     Column(
         horizontalAlignment = Alignment.Start,
@@ -65,30 +62,45 @@ fun EditPage(){
             onValueChange = { description = it },
             label = { Text("Description") }
         )
-        OutlinedTextField(
-            value = dueDateStr,
-            onValueChange = { },
-            label = { Text("Date") },
-            placeholder = { Text("MM/DD/YYYY") },
-            trailingIcon = {
-                Icon(Icons.Default.DateRange, contentDescription = "Select Date")
-            },
-            // Modifier.clickable doesn't work for text fields, so we use Modifier.pointerInput
-            // in the Initial pass to observe events before the text field consumes them
-            // in the Main pass.
-            modifier = Modifier
-                .fillMaxWidth()
-                .pointerInput(datePickerState){
-                    awaitEachGesture {
-                        awaitFirstDown(pass = PointerEventPass.Initial)
-                        val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
-                        if (upEvent != null){
-                            showDatePicker = true
-                        }
+        DueDatePicker(datePickerState)
+        DueTimePicker(timePickerState)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DueDatePicker(datePickerState: DatePickerState){
+    var showDatePicker by remember { mutableStateOf(false) }
+    val dueDateStr = LocalDateTime.ofInstant(
+        Instant.ofEpochMilli(
+            datePickerState.selectedDateMillis ?: Instant.now().toEpochMilli()
+        ),
+        ZoneId.systemDefault()
+    ).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))
+
+    OutlinedTextField(
+        value = dueDateStr,
+        onValueChange = { },
+        label = { Text("Date") },
+        placeholder = { Text("MM/DD/YYYY") },
+        trailingIcon = {
+            Icon(Icons.Default.DateRange, contentDescription = "Select Date")
+        },
+        // Modifier.clickable doesn't work for text fields, so we use Modifier.pointerInput
+        // in the Initial pass to observe events before the text field consumes them
+        // in the Main pass.
+        modifier = Modifier
+            .fillMaxWidth()
+            .pointerInput(datePickerState){
+                awaitEachGesture {
+                    awaitFirstDown(pass = PointerEventPass.Initial)
+                    val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
+                    if (upEvent != null){
+                        showDatePicker = true
                     }
                 }
-        )
-    }
+            }
+    )
 
     if(showDatePicker){
         DatePickerDialog(
