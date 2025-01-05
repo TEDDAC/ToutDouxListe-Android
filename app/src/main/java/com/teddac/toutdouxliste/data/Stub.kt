99a2771d68b7e61424cd1429105082640c3b13fd
TEDDAC <mathisribemont@gmail.com>
2025-01-05 17:15:30 +0100

    Just better stub

--- app/src/main/java/com/teddac/toutdouxliste/data/Stub.kt
+++ app/src/main/java/com/teddac/toutdouxliste/data/Stub.kt
@@ -6,10 +6,10 @@ import java.time.LocalDateTime
 class Stub {
     val tasks = mutableListOf(
         Task(title = "Todo1", description = "Lorem ipsum dolor sit amet", isDone = false, date = LocalDateTime.now()),
-        Task(title = "Clean desktop", description = "delete icons guys", isDone = false, date = LocalDateTime.now()),
-        Task(title = "Another thing to do", description = "thins that you do (by Slynk)", isDone = true, date = LocalDateTime.now()),
-        Task(title = "Cook a tartiflette", description = "Be careful with the four", isDone = true, date = LocalDateTime.now()),
-        Task(title = "Walk the dog", description = "I have a dog ?!", isDone = false, date = LocalDateTime.now()),
-        Task(title = "Cook a raclette", description = "tell me your french without tell it", isDone = true, date = LocalDateTime.now()),
+        Task(title = "Clean desktop", description = "delete icons guys", isDone = false, date = LocalDateTime.parse("2024-04-30T23:58:00")),
+        Task(title = "Another thing to do", description = "thins that you do (by Slynk)", isDone = true, date = LocalDateTime.parse("2024-06-18T12:30:00")),
+        Task(title = "Cook a tartiflette", description = "Be careful with the four", isDone = true, date = LocalDateTime.parse("2024-09-11T09:38:00")),
+        Task(title = "Walk the dog", description = "I have a dog ?!", isDone = false, date = LocalDateTime.parse("2024-05-18T18:25:00")),
+        Task(title = "Cook a raclette", description = "tell me your french without tell it", isDone = true, date = LocalDateTime.parse("2026-12-25T00:00:00")),
     )
 }
